af7b79b5cc2bfad2d4a85d54cee38d17b78917cb
DownTheMatrix <suija87@gmail.com>
2019-01-03 18:23:00 +0100

    Move to dist/

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const MENU = document.querySelector(".navbar__toggle");
-const NAV = document.querySelector(".navbar");
-
-function toggleMenu() {
-    NAV.classList.toggle("active");
-}
-
-MENU.addEventListener("click", toggleMenu);
